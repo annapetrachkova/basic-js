df29c95400b5114eeecf09f81d17d091e412ffea
Анна Петрачкова <p.anytk@mail.ru>
2021-09-29 14:01:29 +0300

    init: done

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,8 +13,11 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (arr.length === 0) return 1
+    else {
+      return Array.isArray(arr) ? Math.max(...arr.map(item => this.calculateDepth(item))) + 1 : 0;
+    }
+
   }
 }
